75e00c44f193595a0d1a8588fd794b6adc749430
Irene201 <56350981+Irene201@users.noreply.github.com>
2021-10-01 12:21:49 +0200

    Add files via upload

---
+++ Uppgift3/script.js
@@ -0,0 +1,6 @@
+let resultBtn = document.querySelector("#resultButton");
+
+resultBtn.addEventListener("click", function(event) {
+    let h = document.querySelector("#hh1");
+    h.innerHTML = "Goodbye World";
+})
